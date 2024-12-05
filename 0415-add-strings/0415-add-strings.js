c125963f1f1794c1ad47bdd039aa25983cbf8534
ramees27 <rameeskt27@gmail.com>
2024-12-05 10:05:07 +0530

    Time: 1 ms (91.94%), Space: 50.2 MB (88.28%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    num1=BigInt(num1)
+     num2=BigInt(num2)
+     let sum=(num1+num2)
+     
+   
+     return String(sum)   
+};
