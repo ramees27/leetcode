1c71faff9031e34036ec73ce59cefbe7c57791b9
ramees27 <rameeskt27@gmail.com>
2024-12-12 10:08:02 +0530

    Time: 126 ms (9.28%), Space: 60.7 MB (44.93%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+   let newArr=nums.sort((a,b)=>a-b)
+    for(let i=0;i<newArr.length;i++){
+        if(newArr[i]===newArr[i+1]){
+            return newArr[i]
+        }
+    }
+}
+
