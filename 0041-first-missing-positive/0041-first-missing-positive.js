4db716b7053f079235df0fca26bb290ef0c1a6e8
ramees27 <rameeskt27@gmail.com>
2024-12-16 10:27:14 +0530

    Time: 43 ms (20.41%), Space: 59 MB (47.01%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let num=1
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<nums.length;i++){
+            if(nums[i]===num){
+              num+=1 
+            }
+        
+    }
+     return num
+};
