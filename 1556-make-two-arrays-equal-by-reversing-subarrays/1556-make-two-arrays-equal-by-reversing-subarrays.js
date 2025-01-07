26fe24b961ccbbf64f8cdc0b9c06339b11a44c53
ramees27 <rameeskt27@gmail.com>
2025-01-07 09:45:10 +0530

    Time: 6 ms (66.43%), Space: 50.6 MB (89.89%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canBeEqual = function(target, arr) {
+  target=target.sort((a,b)=>a-b)
+  arr=arr.sort((a,b)=>a-b)
+ for(let i=0;i<target.length;i++){
+    if(target[i]!==arr[i]){
+        return false
+    }
+ }
+ return true
+};
