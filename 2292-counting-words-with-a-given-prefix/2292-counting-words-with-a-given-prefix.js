bfc3f32f527607e6fbc203bb96b2780617b40d0e
ramees27 <rameeskt27@gmail.com>
2025-01-09 23:52:00 +0530

    Time: 1 ms (32.37%), Space: 49.7 MB (89.6%) - LeetHub

---
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+    return words.reduce((acc,word)=>acc+(word.startsWith(pref)?1:0),0)
+    
+};
